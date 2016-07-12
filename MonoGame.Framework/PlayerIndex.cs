86ba998a6f7d88925495296136f431196f72d196
Sander Verbeek <sanderman@gmail.com>
2016-07-12 14:05:05 +0200

    This change is required for 8 controllers support and didn't get properly included in the previous commit.

---
+++ MonoGame.Framework/PlayerIndex.cs
@@ -0,0 +1,19 @@
+// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+namespace Microsoft.Xna.Framework
+{   
+    public enum PlayerIndex
+    {
+        One = 0,
+        Two = 1,
+        Three = 2,
+        Four = 3,
+		Five = 4,
+		Six = 5,
+		Seven = 6,
+		Eight = 7
+    }
+}
+
